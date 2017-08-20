b4a96ebc2fa82d320833cd02d5c3e57719bac9ad
michael mead <michaelmead007@icloud.com>
2017-08-20 08:02:22 -0500

    add signout button and error catching for user not signed in

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
 'use strict';
 
 function createFlashCard(title, question, answer, language) {
-  var uid = firebase.auth().currentUser.uid;
+  try {
+    var uid = firebase.auth().currentUser.uid;
+    console.log('you passed')
+    // A post entry.
+    var postData = {
+      uid: uid,
+      question: question,
+      answer, answer,
+      title: title,
+      votesUp: 0,
+      votesDown: 0,
+      language: language
+    };
 
-  // A post entry.
-  var postData = {
-    uid: uid,
-    question: question,
-    answer, answer,
-    title: title,
-    votesUp: 0,
-    votesDown: 0,
-    language: language
-  };
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('flashCards').push().key;
 
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('flashCards').push().key;
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/flashCards/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/flashCards/' + newPostKey] = postData;
-  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+    return firebase.database().ref().update(updates);
+  } catch (err) {
+    document.getElementById("error").innerHTML = 'You are not signed in.';
+    console.log(err.message);
+  }
 
-  return firebase.database().ref().update(updates);
 }
 
 
